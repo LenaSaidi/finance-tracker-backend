0f6a1365628014f98a091ccfbaa339b2e8a29518
Lenasaidi <saidilena6@gmail.com>
2026-06-01 23:32:23 +0200

    initialisation

---
+++ src/test/java/com/dauphine/finance_tracker/FinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.finance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
